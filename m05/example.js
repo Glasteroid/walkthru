9269e0f45577a7800f542b2a447de7b64407c9de
Glasteroid <78562569+Glasteroid@users.noreply.github.com>
2023-05-04 22:18:31 -0700

    Update example.js

--- m05/example.js
+++ m05/example.js
@@ -4,18 +4,10 @@ var operator = 'addition'; // Type of calculation
 var i = 1;                 // Set counter to 1
 var msg = '<h2>Multiplication Table</h2>';              // Message
 
-if (operator === 'addition') {
-  // Do addition
-  while (i < 11) {
-    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
-    i++;
-  }
-} else {
-  // Do multiplication
-  while (i < 11) {
-    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
-    i++;
-  }
+// Do multiplication
+while (i < 11) {
+  msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+  i++;
 }
 
 // Write the message into the page
