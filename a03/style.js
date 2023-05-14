60a860908589830e48b4229caf2b56385ba7c308
Glasteroid <78562569+Glasteroid@users.noreply.github.com>
2023-05-13 23:30:23 -0700

    Update style.js

--- a03/style.js
+++ a03/style.js
@@ -56,3 +56,52 @@ rLine +="</p>";
 
 document.getElementById("downRight").innerHTML = rLine;
 }
+
+function upLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+var rLine ="";
+for (i=pHeight;i > 0;i--){
+rLine +="<p>";
+//Create each line on the Rhombus
+for(j=0;j<i - 1;j++){
+
+rLine +="<span style='color:" + "white" + ";'>" + pSymbol +"</span>";
+
+if (i%2)
+//even
+rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+else
+//odd
+rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";  
+  
+}
+rLine +="</p>";
+// console.log(rLine);
+
+}
+
+document.getElementById("upLeft").innerHTML = rLine;
+}
+
+function downLeft(pHeight, pColorEven, pColorOdd, pSymbol){
+var rLine ="";
+for (i=pHeight;i > 0;i--){
+rLine +="<p>";
+//Create each line on the Rhombus
+for(j=0;j<i;j++){
+
+rLine +="<span style='color:" + "white" + ";'>" + pSymbol +"</span>";    
+
+if (i%2)
+//even
+rLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+else
+//odd
+rLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";   
+}
+rLine +="</p>";
+// console.log(rLine);
+
+}
+
+document.getElementById("downLeft").innerHTML = rLine;
+}
