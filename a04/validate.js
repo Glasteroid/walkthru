7bf54455e920f595e11fe9840a62d708733d8499
Glasteroid <78562569+Glasteroid@users.noreply.github.com>
2023-05-25 16:40:22 -0700

    Update validate.js

--- a04/validate.js
+++ a04/validate.js
@@ -1,46 +1,79 @@
-function validateForm() {
-  var errorMessages = ""; // Initialize the error messages variable
-
+function validateFirstName() {
+  var validFirstname=false;
   var firstname = document.getElementById("firstname").value;
-  var lastname = document.getElementById("lastname").value;
-  var userEmail = document.getElementById("email").value;
-  var phone = document.getElementById("phone").value;
-  var username = document.getElementById("username").value;
-  var password = document.getElementById("password").value;
-  var address = document.getElementById("address").value;
-  var city = document.getElementById("city").value;
-  var zipcode = document.getElementById("zipcode").value;
-  var country = document.getElementById("country").value;
-
-  if (firstname === null || firstname === "" || firstname.length > 20)
+  if (firstname==="null" || firstname==="" || firstname.length > 20)
     errorMessages += "<p>The firstname is required and cannot be greater than 20 characters</p>";
+  else
+    validFirstname = true;
+  document.getElementById("errorMessages").innerHTML = errorMessages;
+  return (validFirstname);
+}
 
-  if (lastname === null || lastname === "" || lastname.length > 50)
+function validateLastName() {
+  var validLastname=false;
+  var lastname = document.getElementById("lastname").value;
+  if (lastname==="null" || lastname==="" || lastname.length > 50)
     errorMessages += "<p>The lastname is required and cannot be greater than 50 characters</p>";
+  else
+    validLastname = true;
+  document.getElementById("errorMessages").innerHTML = errorMessages;
+  return (validLastname);
+} 
 
+function validateEmail() {
+  var userEmail = document.getElementById("email").value;
   var atpos = userEmail.indexOf("@");
   var dotpos = userEmail.lastIndexOf(".");
-  if (atpos < 1 || dotpos < atpos + 2 || dotpos + 2 >= userEmail.length)
-    errorMessages += "<p>Invalid email</p>";
+  if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=userEmail.length) {
+    errorMessages = "<p>Invalid email</p>";
+  else
+    return true;
+}
+  
+function validatePhone() {
+  var phone = document.getElementById("phone").value;
+  if (isNaN(phone) || phone.length >15 || phone===null || phone==="")
+    errorMessages = "<p>Invalid phone number </p>";
+  else
+    return true; 
+} 
 
-  if (isNaN(phone) || phone.length > 15 || phone === null || phone === "")
-    errorMessages += "<p>Invalid phone number</p>";
-
-  if (username === "" || username.length > 12)
+function validateUsername() {
+  var validUsername = false;
+  var username = document.getElementById("username").value;
+  if (username==="" || username.length > 12)
     errorMessages += "<p>The username is required and cannot be greater than 12 characters</p>";
+  else 
+    validUsername = true;
+  return (validUsername);
+}  
 
-  if (password === "" || password.length > 7)
+function validatePassword() {
+  var validPassword = false;
+  var password = document.getElementById("password").value;
+  if (password==="" || password.length > 7)
     errorMessages += "<p>The password is required and cannot be greater than 7 characters</p>";
-
-  if (address === null || address === "")
+  else 
+    validPassword = true;
+  return (validPassword);
+}  
+  
+function validateAddress() {
+  var validAddress = false;
+  var address = document.getElementById("address").value;
+  if (address==="null" || address==="")
     errorMessages += "<p>The address is required</p>";
+  else 
+    validAddress = true;
+  return (validAddress);
+}  
 
-  if (city === null || city === "")
+function validateCity() {
+  var validCity = false;
+  var city = document.getElementById("city").value;
+  if (city==="null" || city==="")
     errorMessages += "<p>The city is required</p>";
-
-  if (country === "USA" && zipcode.length > 5)
-    errorMessages += "<p>Must be living in United States and input must only be a maximum of 5 digits</p>";
-
-  document.getElementById("errorMessages").innerHTML = errorMessages; // Display the error messages
-
-  return errorMessages; // Return true if there are no error messages, indicating a valid form
+  else 
+    validCity = true;
+  return (validCity);
+}
