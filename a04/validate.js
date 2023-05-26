ee44b732bf35c19e8b1f4e5989c4c2c836d7ab0d
Glasteroid <78562569+Glasteroid@users.noreply.github.com>
2023-05-25 17:23:20 -0700

    Update validate.js

--- a04/validate.js
+++ a04/validate.js
@@ -73,6 +73,9 @@ function validateForm() {
       validZipcode = true;
     }
   }
+  else {
+    errorMessages += "<p>Must be in the United States to enter zipcode and be a maximum of 5 digits</p>";
+  }
     document.getElementById("errorMessages").innerHTML = errorMessages;
     return (validFirstname && validLastname &&  validemail && validphone && validUsername && validPassword && validAddress && validCity && validZipcode);
 }
